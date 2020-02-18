0783adc6cdfe144b75779e78b50610f45ccb1bec
Dimpy Vaidya <n01406287@nj-00118-010.humber.org>
2020-02-18 17:01:34 -0500

    Made CHanges for heroku to work

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get("/contact", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+const PORT=process.env.PORT||3000;
+app.listen(PORT, () => {
     console.log(`Web server is running`);
 })
